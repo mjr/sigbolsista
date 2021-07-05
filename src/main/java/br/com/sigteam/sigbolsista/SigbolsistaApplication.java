1c6e0bd3c9c4a568c61a40a8daa9ae7cc843a2e1
Manaia Junior <manaiajr.23@gmail.com>
2021-07-05 15:55:59 -0300

    Config database

--- src/main/java/br/com/sigteam/sigbolsista/SigbolsistaApplication.java
+++ src/main/java/br/com/sigteam/sigbolsista/SigbolsistaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SigbolsistaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SigbolsistaApplication.class, args);
 	}
